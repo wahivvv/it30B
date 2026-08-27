a44c25f2cd4dad32c82ee7f25cc406309e6e4af7
wahivvv <wahivwahiv4@agmail.com>
2026-08-27 08:24:57 +0800

    Update Current_timestamp

--- backups/lab3.sql
+++ backups/lab3.sql
@@ -24,3 +24,24 @@ FROM borrow br
     JOIN students s ON br.student_id = s.student_id
     JOIN books b ON br.book_id = b.book_id
 ORDER BY br.borrow_date DESC;
+
+ALTER TABLE borrow
+MODIFY borrow_return_date TIMESTAMP NULL DEFAULT NULL;
+
+UPDATE borrow
+SET borrow_return_date = NULL
+WHERE borrow_return_date = '2026-08-25 08:15:50';
+
+UPDATE borrow
+SET borrow_return_date = CURRENT TIMESTAMP
+WHERE borrow_id = 1 AND borrow_return_date IS NULL;
+
+SELECT br.borrow_id, s.student_id,
+    CONCAT(s.student_first_name, ' ', s.student_last_name) AS student_name,
+    b.book_title, b.book_author, b.book_category,
+    br.borrow_date,br.borrow_return_date
+FROM borrow br
+    JOIN students s ON br.student_id = s.student_id
+    JOIN books b ON br.book_id = b.book_id
+    WHERE br.borrow_return_date IS NOT NULL
+ORDER BY br.borrow_date DESC;
--- it30B/backups/readme.txt
+++ it30B/backups/readme.txt
@@ -11,3 +11,4 @@ VALUES(values);
 Utility Commands
 \! cls
 mysqldump -u root -p --database library_db > "C:\xampp\htdocs\it30B\backups\%date:~-4%%date:~4,2%%date:~7,2%_%time:~0,2%%time:~3,2%%time:~6,2%_08182026_library_db.sql"
+mysqldump -u root -p --database library_db > "C:\xampp\htdocs\it30B\backups%date:~-4%%date:~4,2%%date:~7,2%_%time:~0,2%%time:~3,2%%time:~6,2%_08182026_library_db.sql"
